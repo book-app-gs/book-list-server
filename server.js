692a68d4785ed1e6311ee4775657c5d6925042fa
svivek71@yahoo.com <svivek71@yahoo.com>
2017-12-12 12:47:56 -0800

    index page load

--- server.js
+++ server.js
@@ -3,10 +3,12 @@
 // export PORT=3000
 // export CLIENT_URL=http://localhost:8000
 // export DATABASE_URL=postgres://localhost:5432/task_app
+// export DATABASE_URL=postgres://postgres:sunitha@localhost:5432/books_app
 
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 4040;
@@ -18,7 +20,51 @@ client.on('error', err => console.error(err));
 
 app.use(cors());
 
+app.get('/index', (request, response) => {
+    response.sendFile('index.html', {root: './public'});
+  });
+
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
+//createBooks();
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
+// <<<<<<<<<<<<,<<>>>>>>>>>>>>>>>>>>>
+// function loadBooks() {
+//     client.query('SELECT COUNT(*) FROM books')
+//     .then(result => {
+//       if(!parseInt(result.rows[0].count)) {
+//         fs.readFile('./data/books.json', 'utf8', (err, fd) => {
+//           JSON.parse(fd).forEach(ele => {
+//             client.query(`
+//             INSERT INTO
+//             books (title, author, isbn, image_url, description)
+//             VALUES ($1, $2, $3, $4, $5);
+//           `,
+//             [ele.title, ele.author, ele.isbn, ele.image_url, ele.description])
+
+//             .catch(console.error);
+//           })
+//         })
+//       }
+//     })
+//   }
+
+//   function createBooks() {
+//     client.query(`
+//       CREATE TABLE IF NOT EXISTS books (
+//         book_id SERIAL PRIMARY KEY,
+//         author VARCHAR(50) NOT NULL,
+//         title VARCHAR(50) NOT NULL,
+//         isbn VARCHAR (50),
+//         image_url VARCHAR(500),
+//         description TEXT NOT NULL);`
+//     )
+//       .then(() => {
+//         console.log('created table')
+//         loadBooks();
+//       })
+//       .catch(err => {
+//         console.error(err);
+//       });
+//   }
